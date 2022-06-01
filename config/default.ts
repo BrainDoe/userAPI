f87032b4e83f2f5b193af8fd98412ffdcb39d8f2
Donatus Brain Okwe <braindona@gmail.com>
2022-05-31 20:50:44 -0500

    Completed the setup and initiated healthcheck endpoint.

---
+++ config/default.ts
@@ -0,0 +1,5 @@
+export default {
+  port: 4200,
+  dbUri: "mongodb+srv://donatus:<PASSWORD>@cluster0.rfriq.mongodb.net/userAPI?retryWrites=true&w=majority",
+  logLevel: "info",
+}
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('Hello Wolrd!!!');
+require('dotenv').config();
+import express from 'express';
+import config from "config"
+import dbConnect from './utils/dbConnect';
+import log from './utils/logger';
+import router from './routes';
+
+const app = express();
+
+app.use(router);
+
+const port = config.get('port');
+app.listen(port, () => {
+  log.info(`App listening on port http://localhost:${port}`);
+
+  dbConnect();
+});
